f1dd1e225db363dbf5d7ba3c8fd5454ff3bc70ca
Hedpedz <143394250+Hedpedz@users.noreply.github.com>
2025-02-24 14:00:03 +0100

    Add game and category pages with search functionality and styling

--- Zelda_api/src/components/GameCard.jsx
+++ Zelda_api/src/components/GameCard.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import "../assets/styles/gameCard.scss";
+
+
 export default function GameCard({ game }) {
   return (
-    <article>
+    <article className="gameCard">
       <h3>{game.name}</h3>
       <p>{game.description}</p>
       <span>{game.released_date}</span>
+      <Link to={`games/${game.id}`}>Gå til spill</Link>
     </article>
   );
 }
--- Zelda_api/src/components/Home.jsx
+++ Zelda_api/src/components/Home.jsx
@@ -1,12 +1,36 @@
+import "../assets/styles/layout.scss";
 import GameCard from "./GameCard";
 
 export default function Home({ games }) {
+
+  const [search, setSearch] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClick = async() => {
+    fetch(`https://zelda.fanapis.com/api/games?name=${search}`)
+    .then(response => response.json());
+  };
+
   return (
     <main>
-      <h1>Forside</h1>;
-      {games?.map((game) => (
-        <GameCard key={game.id} game={game} />
-      ))}
+      <h1>Forside</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Her kan du søke etter spill</label>
+        <input type="search" id="search" onChange={handleChange}></input> 
+        <button onClick={handleClick}>Søk etter spill</button>
+        </form>
+      <section className="flex-section">
+        {games?.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </section>
     </main>
   );
 }
